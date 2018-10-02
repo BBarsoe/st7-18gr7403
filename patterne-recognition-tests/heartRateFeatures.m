c4132d3490036c6dcc7118e1ad1f1f80b06440b1
Andreas Lauridsen <andreas_lauridsen@hotmail.com>
2018-10-02 14:18:34 +0200

    Added som documentation

--- patterne-recognition-tests/heartRateFeatures.m
+++ patterne-recognition-tests/heartRateFeatures.m
@@ -10,12 +10,12 @@ heartRateDerivative = diff(heartRate);
 % Wave lenght
 windowSize = 300; %Seconds
 startIndex = 1;
-resultSize = [ceil(seconds(dateTime(end) - dateTime(1)) / 300) 2];
+resultSize = [ceil(seconds(dateTime(end) - dateTime(1)) / windowSize) 2];
 resultVariables = {'double','datetime'};
 result = table('Size',resultSize,'VariableTypes',resultVariables);
 totalTime = 0;
 pointLength = 0;
-tmpResult1 = zeros(ceil(seconds(dateTime(end) - dateTime(1)) / 300),1);
+tmpResult1 = zeros(ceil(seconds(dateTime(end) - dateTime(1)) / windowSize),1);
 tmpResult2 = datetime;
 
 for index = 1:size(heartRateDerivative)-1
@@ -28,16 +28,25 @@ for index = 1:size(heartRateDerivative)-1
     heartRateDiff = diff([heartRateDerivative(index) heartRateDerivative(index+1)]);
     %the length bewteen the two points
     pointLength = pointLength + norm([heartRateDiff timeDiff]);
+    %the total time of current window
     totalTime = totalTime + timeDiff;
+    % If more than 300 seconds has been calculated
     if totalTime/windowSize >= 1
+        %Save the current wavelenght
         tmpResult1(startIndex,1) = pointLength;
+        %Save the current timestamp
         tmpResult2(startIndex,1) = dateTime(index);
+        %reset the wavelenght
         pointLength = 0;
+        %increment the index for saving the wavelenghts
         startIndex = startIndex + 1;
+        %Save the overflowed time
         totalTime = totalTime-windowSize;
     end
 end
+%creates a table with the results
 result = table(tmpResult1(1:end-1),tmpResult2);
+
 
 %% Plot data
 subplot(2,1,1)
@@ -45,4 +54,4 @@ plot(dateTime(1:end-1),heartRateDerivative);
 hold on;
 plot(dateTime,heartRate);
 subplot(2,1,2)
-plot(tmpResult2,tmpResult1(1:end-1))
+plot(result{:,2},result{:,1})
