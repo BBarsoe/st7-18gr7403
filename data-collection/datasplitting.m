953ad6b377d25eb03c227c1aee2558fd67a114fc
erik <smede_gaard@hotmail.com>
2018-10-10 21:31:15 +0200

    Changed data-collection/datasplitting to a functioning build for the whole dataset. Also comments for the script

--- data-collection/datasplitting.m
+++ data-collection/datasplitting.m
@@ -1,23 +1,32 @@
-% clear;clc;close all;
-%Jacob_dataset = importfile('Fitbit-dataset-Jacob.csv',1,393683);
-[minute_number,minutes_time] = discretize(Jacob_dataset.Date,'minute');
-minutes_time = minutes_time';
-Jacob_dataset.minute = minute_number;
- G = findgroups(Jacob_dataset.minute);
- heartsplit = splitapply(@mean,Jacob_dataset.heart,G);
- 
+clear;clc;close all;
+imported_dataset = importfile('Fitbit-dataset-Jacob.csv',1,393683);%Importerer datasættet der bruges
+imported_dataset.Properties.VariableNames{1} = 'Date';%Sørger for at kolonnerne har variabelnavne til scriptet
+imported_dataset.Properties.VariableNames{2} = 'heart';
+[minute_number,minutes_time] = discretize(imported_dataset.Date,'minute');%Inddeler data i minut intervaller
+minutes_time = minutes_time';%Vender matricen fra 1 række til 1 kolonne
+%imported_dataset.minute = minute_number; Ved ikke lige hvorfor den her er der
+G = findgroups(imported_dataset.minute);%Grupperer heart data i minutter
+heartsplit = splitapply(@mean,imported_dataset.heart,G);%gns heart pr. minut
+
 k = 1;
 tmp = 0;
 lasttime = 2;
-data = table(minutes_time,zeros(17280,1));
-for i = 2:10000
+data = table(minutes_time,zeros(length(minutes_time),1));%Laver en matrice med minutter og nuller
+for i = 2:height(imported_dataset)%Hele datasættet uden NaN på række 1
     
-    if(minutes_time(k)>Jacob_dataset.Date(i))
-        tmp = Jacob_dataset.heart(i)+tmp;
+    if (minutes_time(k) >= imported_dataset.Date(i))%Hvis tiden er lavere end optællingsminuttet
+        tmp = imported_dataset.heart(i)+tmp;%Lægger heart sammen med tidligere tal
+        
+    elseif (imported_dataset.Date(i) > minutes_time(k)+minutes(1))%Hvis der er mere end 1 minut mellem datapunkter
+        while (imported_dataset.Date(i) > minutes_time(k)+minutes(1))
+            tmp = imported_dataset.heart(i);
+            k = k+1;%Tæller k op uden at inkrementere i så vi ikke kommer ud af sync
+            lasttime = i;%Så der ikke divideres med den tidligere i (fra if->elseif)
+        end
     else
-            data{k,2} = tmp/(i-lasttime);
-            tmp = Jacob_dataset.heart(i);
-            k = k+1;
-            lasttime = i;
+        data{k,2} = tmp/(i-lasttime);%Tager gns af minuttets datapunkter
+        tmp = imported_dataset.heart(i);%Nulstiller tmp til nuværende heart
+        k = k+1;%Øger tællingsminuttet
+        lasttime = i;%Nulstiller nævneren
     end
 end
