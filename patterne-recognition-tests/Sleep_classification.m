697d3337fb9b1f908f42045b68bb0f849aa5e359
Jacobponsaing <36076707+Jacobponsaing@users.noreply.github.com>
2018-10-24 10:38:55 +0200

    Changed name

--- patterne-recognition-tests/Sleep_classification.m
+++ patterne-recognition-tests/Sleep_classification.m
@@ -3,11 +3,11 @@ close all
 clc
 %% Load data
 
-imported_dataset = importfile('Heart_2009-0110_Jacob.csv',1,393684);%Importerer datasættet der bruges
-imported_dataset.Properties.VariableNames{1} = 'Date';%Sørger for at kolonnerne har variabelnavne til scriptet
+imported_dataset = importfile('Heart_2009-0110_Jacob.csv',1,393684);%Importerer datasÃ¦ttet der bruges
+imported_dataset.Properties.VariableNames{1} = 'Date';%SÃ¸rger for at kolonnerne har variabelnavne til scriptet
 imported_dataset.Properties.VariableNames{2} = 'heart';
 [minute_number,minutes_time] = discretize(imported_dataset.Date,'minute');%Inddeler data i minut intervaller
-minutes_time = minutes_time';%Vender matricen fra 1 række til 1 kolonne
+minutes_time = minutes_time';%Vender matricen fra 1 rÃ¦kke til 1 kolonne
 %imported_dataset.minute = minute_number; Ved ikke lige hvorfor den her er der
 G = findgroups(minute_number);%Grupperer heart data i minutter
 heartsplit = splitapply(@mean,imported_dataset.heart,G);%gns heart pr. minut
@@ -16,22 +16,22 @@ k = 1;
 tmp = 0;
 lasttime = 2;
 data = table(minutes_time,zeros(length(minutes_time),1));%Laver en matrice med minutter og nuller
-for i = 2:height(imported_dataset)%Hele datasættet uden NaN på række 1
+for i = 2:height(imported_dataset)%Hele datasÃ¦ttet uden NaN pÃ¥ rÃ¦kke 1
     
-    if (minutes_time(k) >= imported_dataset.Date(i))%Hvis tiden er lavere end optællingsminuttet
-        tmp = imported_dataset.heart(i)+tmp;%Lægger heart sammen med tidligere tal
+    if (minutes_time(k) >= imported_dataset.Date(i))%Hvis tiden er lavere end optÃ¦llingsminuttet
+        tmp = imported_dataset.heart(i)+tmp;%LÃ¦gger heart sammen med tidligere tal
         
     elseif (imported_dataset.Date(i) > minutes_time(k)+minutes(1))%Hvis der er mere end 1 minut mellem datapunkter
         while (imported_dataset.Date(i) > minutes_time(k)+minutes(1))
             tmp = imported_dataset.heart(i);
-            k = k+1;%Tæller k op uden at inkrementere i så vi ikke kommer ud af sync
-            lasttime = i;%Så der ikke divideres med den tidligere i (fra if->elseif)
+            k = k+1;%TÃ¦ller k op uden at inkrementere i sÃ¥ vi ikke kommer ud af sync
+            lasttime = i;%SÃ¥ der ikke divideres med den tidligere i (fra if->elseif)
         end
     else
         data{k,2} = tmp/(i-lasttime);%Tager gns af minuttets datapunkter
-        tmp = imported_dataset.heart(i);%Nulstiller tmp til nuværende heart
-        k = k+1;%Øger tællingsminuttet
-        lasttime = i;%Nulstiller nævneren
+        tmp = imported_dataset.heart(i);%Nulstiller tmp til nuvÃ¦rende heart
+        k = k+1;%Ã˜ger tÃ¦llingsminuttet
+        lasttime = i;%Nulstiller nÃ¦vneren
     end
 end
 
