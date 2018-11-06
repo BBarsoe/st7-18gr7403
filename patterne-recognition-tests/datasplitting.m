af8c6233a58ec667012e3893facb8387d6d935ea
erik <smede_gaard@hotmail.com>
2018-11-06 12:11:22 +0100

    Added female test subject data for 16th of september to 2nd of november. Changed datasplitting to work on larger data files.

--- patterne-recognition-tests/datasplitting.m
+++ patterne-recognition-tests/datasplitting.m
@@ -1,9 +1,13 @@
-clear;clc;close all;
-imported_dataset = importfile('Heart_0110-0410_Jacob.csv',1,393683);%Importerer datasættet der bruges
+%clear;clc;close all;
+%imported_dataset = importfile('female_data_heart.csv',1,500000);%Importerer datasættet der bruges
 imported_dataset.Properties.VariableNames{1} = 'Date';%Sørger for at kolonnerne har variabelnavne til scriptet
 imported_dataset.Properties.VariableNames{2} = 'heart';
-[minute_number,minutes_time] = discretize(imported_dataset.Date,'minute');%Inddeler data i minut intervaller
-minutes_time = minutes_time';%Vender matricen fra 1 række til 1 kolonne
+[minute_number1,minutes_time1] = discretize(imported_dataset.Date(1:400000),'minute');%Inddeler data i minut intervaller
+[minute_number2,minutes_time2] = discretize(imported_dataset.Date(400001:end),'minute');%Inddeler data i minut intervaller
+minutes_time1 = minutes_time1';%Vender matricen fra 1 række til 1 kolonne
+minutes_time2 = minutes_time2';%Vender matricen fra 1 række til 1 kolonne
+minute_number = [minute_number1;minute_number2];
+minutes_time = [minutes_time1;minutes_time2];
 %imported_dataset.minute = minute_number; Ved ikke lige hvorfor den her er der
 G = findgroups(minute_number);%Grupperer heart data i minutter
 heartsplit = splitapply(@mean,imported_dataset.heart,G);%gns heart pr. minut
