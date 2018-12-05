95829937253e943042fdb6ce412107486291ee93
Andreas Lauridsen <andreas_lauridsen@hotmail.com>
2018-12-05 14:22:32 +0100

    msg

--- complete-dataprocessing/water-model/fluidModel.m
+++ complete-dataprocessing/water-model/fluidModel.m
@@ -1,79 +1,4 @@
 function fluidModel = fluidModel(sleepData,fluidData,heartData,gender,startTime,endTime)
-% %% Convert, trim and sort data raw data
-% %Create a table with sleepdata. 
-% sleepData = table(...
-%     'Size',[height(rawSleep) 2],...
-%     'VariableTypes',["datetime","datetime"],...
-%     'VariableNames',["BedTime","WakeTime"]);
-% sleepData{:,1} = rawSleep{:,1};
-% sleepData{:,2} = rawSleep{:,2};
-% clear rawSleep
-% 
-% %Create a table with fluid journal data
-% fluidData = table(...
-%     'Size',[height(rawFluid) 2],...
-%     'VariableTypes',["datetime","double"],...
-%     'VariableNames',["Time","Amount"]);
-% fluidData{:,1} = rawFluid{:,1};
-% fluidData{:,2} = rawFluid{:,2};
-% clear rawFluid
-% 
-% %Table with heartRateData
-% rawHeartTimeTable = table2timetable(rawHeart);
-% resizedHeartTimeTable = retime(rawHeartTimeTable,'minutely','mean');
-% heartData = timetable2table(resizedHeartTimeTable);
-% clear rawHeart rawHeartTimeTable resizedHeartTimeTable
-% 
-% %% Finding the start and ending times of the simulation.
-% % Neither fluid, sleep or heart data has the same exsact beginning. A
-% % identic start time is needed as guessing whats happenening at time
-% % without data is a bad ideá. As all three datasets begin within a couble
-% % of day of eachother, the latest start time is found and chosen as the
-% % offcial start time.
-% 
-% startTime = max([sleepData{1,2} fluidData{1,1} heartData{1,1}]);
-% 
-% % The same problem persistest with the ending, whereas guessing the missing
-% % values in some dataset is a bad ideá. Here the official ending is chosen
-% % as being the earliest time.
-% endTime = min([sleepData{end,2} fluidData{end,1} heartData{end,1}]);
-% 
-% %% Syncing and cutting the data to match in time
-% % To make sure the data has the same start time and end time, all data 
-% % prior to startTime is removed, and the same with all data after endTime.
-% % This is done by first comparing the startTime with a complete date 
-% % dataset, which returns a vector, the same size as the original date 
-% % dataset, with one and zeroes stateing which date are before the 
-% % startTime. Then the function find() is used which searches for the last 
-% % instance of the values 1, and returns it's index. Then whether or not 
-% % that vector is empty or not, determins if the specific dataset needs 
-% % trimming or not. This process repeated for endTime, as well the two other
-% % datasets.
-% 
-% % The dataset of sleep is tested and if nececary, trimmed.
-% if ~isempty(find(sleepData{:,2} < startTime,1,'last'))
-%     sleepData(1:find(sleepData{:,2} < startTime,1,'last'),:) = [];
-% end
-% if ~isempty(find(sleepData{:,2} > endTime,1,'last'))
-%     sleepData(find(sleepData{:,2} > startTime,1,'last'):end,:) = [];
-% end
-% 
-% % The dataset of fluid jounrnal data is tested and if nececary, trimmed.
-% if ~isempty(find(fluidData{:,1} < startTime,1,'last'))
-%     fluidData(1:find(fluidData{:,1} < startTime,1,'last'),:) = [];
-% end
-% if ~isempty(find(fluidData{:,1} > endTime,1,'last'))
-%     fluidData(find(fluidData{:,1} > endTime,1,'last'):end,:) = [];
-% end
-% 
-% % The dataset of heart rate data is tested and if nececary, trimmed.
-% if ~isempty(find(heartData{:,1} < startTime,1,'last'))
-%     heartData(1:find(heartData{:,1} < startTime,1,'last'),:) = [];
-% end
-% if ~isempty(find(heartData{:,1} > endTime,1,'last'))
-%     heartData(find(heartData{:,1} > endTime,1,'last'):end,:) = [];
-% end
-
 %% Create 'eating'- and 'sleeping'-tables
 %Eating table
 eatingDistribution = [0,0,0,0,0,0,1,4,2,2,2,2,4,2,2,2,2,1,4,1,1,1,0,0];
@@ -166,12 +91,6 @@ for i = 1:length(bodyWaterVolume)
 %     " | UO = " + urineOutput(heartData{i,2},bodyWaterVolume(i))
 end
 
-
-%% Plot
 S = startTime:minutes(1):endTime;
 fluidModel = table(S,bodyWaterVolume);
-% plot(S,bodyWaterVolume)
-% grid on;
-% xlabel('Time (minutes)'); ylabel('Fluid balance (ml)');
-% title('Model of Jacobs Fluid Balance');
 end
